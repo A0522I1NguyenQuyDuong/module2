b5ceb1ceae85b8d3a7c5574a9593520ff06fd529
A0522I1NguyenQuyDuong <quyduongn524@gmail.com>
2022-08-12 18:51:14 +0700

    capnhat

--- src/XoaPhanTuTrongMang.java
+++ src/XoaPhanTuTrongMang.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class XoaPhanTuTrongMang {
+import java.util.Scanner;
+
+public class XoaPhanTuTrongMang {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Mảng trước khi xóa");
+        for (int i = 0;i < array.length;i++) {
+            System.out.print(array[i] + ", ");
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập phần tử cần xóa");
+        int num_Del = sc.nextInt();
+        for (int i = 0;i < array.length;i++) {
+            if (array[i] == num_Del) {
+                System.out.println("Index Del: " + i);
+                for (int j = i;j < array.length - 1;j++) {
+                    array[j] = array[j+1];
+                }
+                array[array.length - 1] = 0;
+            }
+        }
+        System.out.println("Mảng sau khi xóa");
+        for (int i = 0;i < array.length;i++) {
+            System.out.print(array[i] + ", ");
+        }
+    }
 }
