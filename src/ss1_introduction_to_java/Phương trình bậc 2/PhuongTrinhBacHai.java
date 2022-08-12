7f14ec555f137ac36ef9fd84e9dac6f1c4886890
A0522I1NguyenQuyDuong <quyduongn524@gmail.com>
2022-08-12 18:18:19 +0700

    capnhat

---
+++ src/ss1_introduction_to_java/Phương trình bậc 2/PhuongTrinhBacHai.java
@@ -0,0 +1,25 @@
+package ss1_introduction_to_java;
+
+import java.util.Scanner;
+
+public class PhuongTrinhBacHai {
+    public static void main(String[] args) {
+        double a,b,c;
+        System.out.println("Nhập a,b,c");
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        c = sc.nextDouble();
+        QuadraticEquation qe = new QuadraticEquation(a, b, c);
+        if (qe.getDiscriminant()<0){
+            System.out.println("Phương trình không có nghiệm nguyên");
+        } else if (qe.getDiscriminant()==0) {
+            System.out.println("Phương trình có nghiệm nguyên: " + qe.getRoot1());
+        }else
+                System.out.println("Phương trình có 2 nghiệm:");
+                System.out.println("Denta là : " +qe.getDiscriminant());
+                System.out.println("Nghiệm1:" +qe.getRoot1());
+                System.out.println("Nghiệm 2:" +qe.getRoot2());
+            }
+            
+        }
