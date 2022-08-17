c5d8feab0d3e0db17626ca3d851b14ec0f5ea50c
A0522I1NguyenQuyDuong <quyduongn524@gmail.com>
2022-08-17 13:19:18 +0700

    capnhat

---
+++ src/AccessModifier/Circle.java
@@ -0,0 +1,21 @@
+package AccessModifier;
+
+public class Circle {
+    private double radius = 1;
+    private String color = "black";
+    private double pi = 3.1412;
+
+    public Circle(){
+
+    }
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea(){
+        return this.radius * this.radius * pi;
+    }
+}
