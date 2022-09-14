5c006693a2726130ea7e323bcbb71a586cb4666e
A0522I1NguyenQuyDuong <106537672+A0522I1NguyenQuyDuong@users.noreply.github.com>
2022-09-14 18:33:31 +0700

    Add files via upload

---
+++ src/TuTrongChuoi.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
+public class TuTrongChuoi {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Input string: \n");
+        String str = sc.nextLine();
+        String[] array = str.split(" ");
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < array.length;i++) {
+            String key = array[i];
+            if (map.containsKey(key)) {
+                Integer value = map.get(key);
+                map.remove(key);
+                map.put(key, value + 1);
+            } else {
+                map.put(key, 1);
+            }
+        }
+
+        Set<String> set = map.keySet();
+        for (String key : set) {
+            System.out.println(key + " : " + map.get(key) + " lần.\n");
+        }
+    }
+}
