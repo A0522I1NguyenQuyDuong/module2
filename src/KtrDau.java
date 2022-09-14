cf41b932dae4efad3235e170126a6c023c3eef24
A0522I1NguyenQuyDuong <106537672+A0522I1NguyenQuyDuong@users.noreply.github.com>
2022-09-14 19:11:50 +0700

    Add files via upload

---
+++ src/KtrDau.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class KtrDau {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Expression: \n");
+        String str = sc.nextLine();
+        Stack leftBracket = new Stack<>();
+        Stack rightBracket = new Stack<>();
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == '('){
+                leftBracket.push(str.charAt(i));
+            } else if (str.charAt(i) == ')') {
+                rightBracket.push(str.charAt(i));
+                
+            }
+        }
+        if (leftBracket.size() == rightBracket.size()){
+            System.out.println("Valid");
+        }else {
+            System.out.println("Invalid");
+        }
+    }
+}
